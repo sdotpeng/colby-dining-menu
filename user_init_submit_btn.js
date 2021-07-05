19830faf03839072ee4dada87b6ac0573974f289
Ruize Li <ruize.li@Yahoo.com>
2021-07-05 19:55:35 -0400

    Initial launch
    Implemented the outmost htmls, js, css

---
+++ user_init_submit_btn.js
@@ -0,0 +1,15 @@
+/**
+ * Action after user initially clicks on the button
+ * @Todo remove everything on the page -> render the page
+ */
+
+const btn = document.getElementById("user-input-btn");
+
+// on click the button
+btn.addEventListener('click', function() {
+    // remove the button
+    const main = document.getElementById("main-body");
+    main.innerHTML = "";
+    // render new page
+    
+})
